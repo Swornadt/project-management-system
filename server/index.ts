35db2f8425daac5d0787fb3da0e91683f06d7dbc
Swornadt <tuladharsworna@gmail.com>
2026-09-02 14:20:10 +0545

    feat: backend structure

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Base API is running');
+});
+
+// Put routes here
+// app.use('/api/events', eventsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
